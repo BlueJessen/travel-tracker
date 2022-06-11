2e622b24971489152516f3e31730d0b767fb8928
Blue Jessen <canealisblue@gmail.com>
2022-06-10 19:15:51 -0700

    Debug information pulling for user

--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,17 @@ import Traveler from './Traveler.js';
 import TravelerRepo from './TravelerRepo.js'
 import Destination from './Destination.js'
 import DestinationRepo from './DestinationRepo.js'
-import Trips from './Trips.js';
+import TripRepo from './TripRepo.js';
+import Trip from './Trip.js';
 import { getPromise, allData } from './apiCalls';
+
+//Query selectors ================================
+let upcomingTrips = document.querySelector('.upcoming-trips-container');
+let pastTrips = document.querySelector('.past-trips-container');
+let pendingTrips = document.querySelector('.pending-trips-container');
+let totalThisYear = document.querySelector('.total-amount-container');
+let topNav = document.querySelector('.top-nav');
+
 
 //Global Variables ===============================
 let currentUser = null;
@@ -15,11 +24,24 @@ window.addEventListener('load', () => {
   allData.then(data => {
     let trips = data[0].trips;
     let destinations = data[1].destinations;
-    let travelers = data[2].travlers;
+    let travelers = data[2].travelers;
+    setInitialData(trips, destinations, travelers);
   }).catch(error => console.log(error));
 });
 
 //Data Functions =====================================
+const setInitialData = (trips, destinations, travelers) => {
+  setUpTravelerRepo(travelers);
+  findUsersTrips(currentUser.id, setUpTripsRepo(trips));
+  console.log(currentUser);
+  console.log(usersTrips);
+}
+
+const getRandomUser = (travelers) => {
+  let randomIndex = Math.floor(Math.random() * travelers.travelers.length)
+  currentUser = travelers.travelers[randomIndex];
+}
+
 const setUpTravelerRepo = (travelersArray) => {
   let travelers = travelersArray.map((traveler) => {
     return new Traveler(traveler);
@@ -29,22 +51,19 @@ const setUpTravelerRepo = (travelersArray) => {
   return travelerRepo
 }
 
-const getRandomUser = (travelers) => {
-  let randomIndex = Math.floor(Math.random() * travelers.travelers.length)
-  currentUser = travelers.travelers[randomIndex];
-}
-
 const setUpTripsRepo = (trips) => {
-  return new Trips(trips);
+  return new TripRepo(trips);
 }
 
 const findUsersTrips = (userID, trips) => {
   usersTrips = trips.findAllTripsByUser(userID);
 }
 
-const setUpDestinationsRepo = (destinations) {
+const setUpDestinationsRepo = (destinations) => {
   let destinationsArray = destinations.map((destination) => {
     return new Destination(destination);
   })
   let destinationRepo = new DestinationRepo(destinationsArray);
 }
+
+//DOM functions ==============================
