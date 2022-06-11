50b24ed2770eafa424132ba435a73b0eb55a978a
Blue Jessen <canealisblue@gmail.com>
2022-06-10 18:08:38 -0700

    Add test for trip class

--- test/trip-test.js
+++ test/trip-test.js
@@ -14,8 +14,40 @@ describe('TripRepo', () => {
     expect(Trip).to.be.a('function');
   });
 
-  it('should be an instance of Trips', function () {
+  it('should be an instance of Trip', function () {
     expect(trip).to.be.an.instanceof(Trip);
+  });
+
+  it('should have an ID', function () {
+    expect(trip.id).to.equal(4);
+  });
+
+  it('should have a users ID', function () {
+    expect(trip.userID).to.equal(2);
+  });
+
+  it('should have a destinationID', function () {
+    expect(trip.destinationID).to.equal(2);
+  });
+
+  it('should have travelers', function () {
+    expect(trip.travelers).to.equal(2);
+  });
+
+  it('should have a date', function () {
+    expect(trip.date).to.equal('2022/02/25');
+  });
+
+  it('should have a duration', function () {
+    expect(trip.duration).to.equal(10);
+  });
+
+  it('should have a status', function () {
+    expect(trip.status).to.equal(2);
+  });
+
+  it('should have suggested activties', function () {
+    expect(trip.suggestedActivities).to.deep.equal([]);
   });
 
 });
